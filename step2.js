3a8097367d760a1445f94048a3d28459adbfc08c
Joel Tannas <jtannas@gmail.com>
2018-01-20 19:27:56 -0700

    complete step 2

--- step2.js
+++ step2.js
@@ -1,1 +1,26 @@
+const https = require('https');
 
+const getAndPrintHtml = function getAndPrintAnHtmlRequest() {
+  const requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+
+  https.get(requestOptions, function(response) {
+    response.setEncoding('utf8');
+
+    let buffer = '';
+    response.on('data', function(data) {
+      buffer += data.toString();
+    });
+    response.on('end', function() {
+      console.log(buffer);
+    });
+
+    response.on('error', function(err) {
+      console.log(err);
+    });
+  });
+};
+
+getAndPrintHtml();
