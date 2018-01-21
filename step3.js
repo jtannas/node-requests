b6e06f5eec44003ec3bb3e49abe55b94e10894ef
Joel Tannas <jtannas@gmail.com>
2018-01-20 19:32:49 -0700

    complete step 3

--- step3.js
+++ step3.js
@@ -1,1 +1,27 @@
+const https = require('https');
 
+const getAndPrintHtml = function makeAndPrintHtmlRequest(options) {
+
+  https.get(options, function(response) {
+    response.setEncoding('utf8');
+
+    let buffer = '';
+    response.on('data', function(data) {
+      buffer += data.toString();
+    });
+    response.on('end', function() {
+      console.log(buffer);
+    });
+
+    response.on('error', function(err) {
+      console.log(err);
+    });
+  });
+};
+
+const requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+getAndPrintHtml(requestOptions);
